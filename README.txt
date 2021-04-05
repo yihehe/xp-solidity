d4f5df1b84f6d0d9af58cf4132876a194216dd82
Stephen He <yihehe@gmail.com>
2021-04-04 20:13:24 -0400

    impl grant, request, verify

--- README.txt
+++ README.txt
@@ -1,18 +1,12 @@
-REMIX EXAMPLE PROJECT
+Verifying Education and Experience
 
-Remix example project is present when Remix loads very first time or there are no files existing in the File Explorer. 
-It contains 3 directories:
+The recruiting process for any organization is long and rigorous, and one of the most time consuming steps is verifying what the applicant says is true, which typically requires background checks and calling on their references. What we will explore in this project is solving the problem verifying an individual's educational and professional experience in a trusted and verifiable way using blockchains.
 
-1. 'contracts': Holds three contracts with different complexity level, denoted with number prefix in file name.
-2. 'scripts': Holds two scripts to deploy a contract. It is explained below.
-3. 'tests': Contains one test file for 'Ballot' contract with unit tests in Solidity.
+|     Caller     | Function |                                 What it does                                  |                   Inputs                    |                    Storage                     | TX? |
+|----------------|----------|-------------------------------------------------------------------------------|---------------------------------------------|------------------------------------------------|-----|
+| Candidate C    | propose  | Propose some experience O at organization O                                   | Organization O address Hash of experience E | Experience { Organization Hash Not certified } | yes |
+| Organization O | certify  | Certifies the hash of some experience E                                       | Hash of experience E                        | Experience { Organization Hash Certified }     | yes |
+| Candidate C    | grant    | Grants permission to Organization R by providing the encrypted list of hashes | Encrypted list of hashes                    | Permission { Encrypted }                       | yes |
+| Organization R | request  | Fetch the latest encrypted hash                                               | Candidate address                           |                                                | no  |
+| Organization R | verify   | Verifies a list of hashes has been certified                                  | Candidate C address List of hashes          |                                                | no  |
 
-SCRIPTS
-
-The 'scripts' folder contains example async/await scripts for deploying the 'Storage' contract.
-For the deployment of any other contract, 'contractName' and 'constructorArgs' should be updated (along with other code if required). 
-Scripts have full access to the web3.js and ethers.js libraries.
-
-To run a script, right click on file name in the file explorer and click 'Run'. Remember, Solidity file must already be compiled.
-
-Output from script will appear in remix terminal.
--- contracts/Xp.sol
+++ contracts/Xp.sol
@@ -3,68 +3,57 @@
 pragma solidity ^0.8.3;
 
 /**
- * @title Xp
+ * @title Xp History
  * @dev Main interface for interacting with a profile
  */
 contract Xp {
     
     mapping(address => Profile) profiles;
     
-    address payable wallet;
-    
     struct Profile {
         Experience[] _experiences;
-        // int zxcv;
         mapping(address => Permission) _permissions;
     }
     
     struct Experience {
         uint256 _id;
         address _organization;
-        string _somethingToPersist;
+        string _hash; // which hash? keccak256?
         bool _isCertified;
     }
     
     struct Permission {
-        TimeRange latestTimeRange;
-        TimeRange[] historyTimeRange;
+        Grant _latestGrant;
+        Grant[] _historyGrants;
     }
     
-    struct TimeRange {
-        uint256 startTime;
-        uint256 endTime;
-    }
-    
-    // takes the hash of an experience
-    // currently is only "somethingToPersist", but can be a more complex object
-    function hashExp(Experience memory exp) internal pure returns(bytes32) {
-        return keccak256(abi.encodePacked(exp._somethingToPersist));
+    struct Grant {
+        uint256 _grantTime;
+        string _encryptedHashes;
     }
     
     // called by candidate
-    function propose(address _organization, string memory _somethingToPersist) external returns(bytes32) {
-        // Profile calldata p = profiles[msg.sender];
+    function propose(address _organization, string memory _hash) external {
         // verify candidate is not organization
+        // dedupe
         
         Experience memory experience = Experience(
             profiles[msg.sender]._experiences.length,
             _organization,
-            _somethingToPersist,
+            _hash,
             false
         );
         profiles[msg.sender]._experiences.push(experience);
-        
-        return hashExp(experience);
     }
     
     // called by organization
-    function certify(address _candidate, bytes32 hash) external {
+    function certify(address _candidate, string memory _hash) external {
         bool certified;
         for (int i = int(profiles[_candidate]._experiences.length) - 1; i >= 0; i--) {
             if (
                 !profiles[_candidate]._experiences[uint(i)]._isCertified &&
                 profiles[_candidate]._experiences[uint(i)]._organization == msg.sender &&
-                hashExp(profiles[_candidate]._experiences[uint(i)]) == hash) {
+                stringEqual(profiles[_candidate]._experiences[uint(i)]._hash, _hash)) {
                     profiles[_candidate]._experiences[uint(i)]._isCertified = true;
                     certified = true;
                     return;
@@ -74,11 +63,53 @@ contract Xp {
         require(certified, "no experience has matched hash");
     }
     
-    // called by requesting organization
-    function getHistory(address _candidate) external view returns(Experience[] memory) {
-        // only return certified history
-        // only if _permissions
+    // called by candidate
+    function grant(address _organization, string memory _encryptedHashes) external {
+        if (profiles[msg.sender]._permissions[_organization]._latestGrant._grantTime != 0) {
+            profiles[msg.sender]._permissions[_organization]._historyGrants.push(
+                profiles[msg.sender]._permissions[_organization]._latestGrant
+            );
+        }
         
-        return profiles[_candidate]._experiences;
+        profiles[msg.sender]._permissions[_organization]._latestGrant._grantTime = block.timestamp;
+        profiles[msg.sender]._permissions[_organization]._latestGrant._encryptedHashes = _encryptedHashes;
+    }
+    
+    // called by requesting organization
+    function request(address _candidate) external view returns(Grant memory) {
+        return profiles[_candidate]._permissions[msg.sender]._latestGrant;
+    }
+    
+    // called by requesting organization
+    // not efficient, but run off chain (view)
+    function verify(address _candidate, string[] memory hashes) external view returns(bool) {
+        for (uint i = 0; i < hashes.length; i++) {
+            bool hashVerified = false;
+            for (uint j = 0; j < profiles[_candidate]._experiences.length; j++) {
+                if (
+                    profiles[_candidate]._experiences[j]._isCertified &&
+                    stringEqual(hashes[i], profiles[_candidate]._experiences[j]._hash)
+                ) {
+                    hashVerified = true;
+                    break;
+                }
+            }
+            // require(hashVerified, "hash could not be verified for candidate");
+            if (!hashVerified) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    // takes the hash of an experience
+    // not used?
+    function hashExp(Experience memory exp) internal pure returns(bytes32) {
+        return keccak256(abi.encodePacked(exp._hash));
+    }
+    
+    // https://docs.soliditylang.org/en/latest/types.html#bytes-and-strings-as-arrays
+    function stringEqual(string memory s1, string memory s2) internal pure returns(bool) {
+        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
     }
 }
